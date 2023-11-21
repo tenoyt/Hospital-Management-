8161be8a51fc0eb06a9d7837cc9ac63d89117d55
rushirajsinh rathod <109704652+rushirajrathod@users.noreply.github.com>
2023-11-21 16:15:45 -0500

    Update receptionist.php
    same dimension comment - try

--- receptionist.php
+++ receptionist.php
@@ -2,7 +2,7 @@
 <html lang="en">
 <head>
     <title>Homepage</title>
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1">
     <link rel="stylesheet" type="text/css" href="https://cdn.jsdelivr.net/npm/bootstrap@4.6.2/dist/css/bootstrap.min.css">
     <link rel="stylesheet" href="receptionist.css">
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.3/css/all.min.css">
