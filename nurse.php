8bafef6ba7cc8ade8b0df2c230fe2f1c1403bb9f
ShivamPatel1408 <146132518+ShivamPatel1408@users.noreply.github.com>
2023-11-21 01:28:29 -0500

    Add files via upload

---
+++ nurse.php
@@ -0,0 +1,58 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <title>Homepage</title>
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" type="text/css" href="https://cdn.jsdelivr.net/npm/bootstrap@4.6.2/dist/css/bootstrap.min.css">
+    <link rel="stylesheet" href="receptionist.css">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.3/css/all.min.css">
+
+</head>
+<body>
+<div class="dashboard-header">
+    <h2>Nurse Dashboard</h2>
+</div>
+
+<div class="container">
+    <div class="flex-container">
+        <div class="sidebar">
+            <div class="top-button">
+                <i class="fas fa-user"></i> <p>Nurse</p>
+            </div>
+            
+            <div id="main">
+                <a href="homepage.php">Patient Info</a>
+                <a href="registered.php">Patient Report</a>
+                <a href="registered.php">Lab Report</a>
+            </div>
+        </div>
+
+        <div class="content">
+            <div class="card-container">
+                <div class="card">
+                    <div class="card-body">
+                        <h5 class="card-title">Card Example 1</h5>
+                        <p class="card-text">This is some content inside the card.</p>
+                    </div>
+                </div>
+
+                <div class="card">
+                    <div class="card-body">
+                        <h5 class="card-title">Card Example 2</h5>
+                        <p class="card-text">This is some content inside the card.</p>
+                    </div>
+                </div>
+
+                <div class="card">
+                    <div class="card-body">
+                        <h5 class="card-title">Card Example 3</h5>
+                        <p class="card-text">This is some content inside the card.</p>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
+
+</body>
+</html>
