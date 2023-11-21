ba23261097b3b0f7c277e8b4de9c45c5eacb2ee3
ShivamPatel1408 <146132518+ShivamPatel1408@users.noreply.github.com>
2023-11-20 22:09:08 -0500

    Add files via upload

---
+++ costcentre.php
@@ -0,0 +1,42 @@
+<html>
+<head>
+    <title> Cost Centre </title>
+    <link rel="stylesheet" type="text/css"
+        href="https://cdn.jsdelivr.net/npm/bootstrap@4.6.2/dist/css/bootstrap.min.css">
+    <link rel="stylesheet" href="costcentre.css">
+</head>
+<body>
+    <div id="main">
+        <p> Cost Centre </p>
+</div>
+    <table>
+        <tr>
+            <th>ID</th>
+            <th>NAME</th>
+            <th>DESCRIPTION</th>
+            <th>COST</th>
+        </tr>
+        <?php
+        $conn = mysqli_connect("localhost","root","1234","project");
+        if ($conn-> connect_error) {
+            die("Connection Failed:". $conn-> connect_error);
+        }
+        $sql = "select COST_CENTRE_ID, ITEM_NAME, ITEM_DESCRIPTION, ITEM_COST from COST_CENTRE";
+        $result = $conn-> query($sql);
+        if ($result-> num_rows > 0) {
+            while ($row = $result-> fetch_assoc()) {
+                echo "<tr><td>". $row["COST_CENTRE_ID"] ."</td><td>". $row["ITEM_NAME"] ."</td><td>". $row["ITEM_DESCRIPTION"] ."</td><td>" .$row["ITEM_COST"] ."</td></tr>";
+            }
+            echo "</table>";
+        }
+        else{
+            echo "0 result";
+        }
+        $conn-> close();
+        ?>
+    </table>
+    <div class="sidebar">
+    <a href="homepage.php">Home</a>
+    </div>
+</body>
+</html
