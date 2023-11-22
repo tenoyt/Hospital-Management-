8150155ef8ae4989cadfa7cf187f4eb2b5c6e7a8
rushirajsinh rathod <109704652+rushirajrathod@users.noreply.github.com>
2023-11-22 17:20:48 -0500

    Add files via upload

---
+++ app_physician.php
@@ -0,0 +1,54 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <title>Dashboard</title>
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta charset="UTF-8">
+    <link rel="stylesheet" type="text/css" href="https://cdn.jsdelivr.net/npm/bootstrap@4.6.2/dist/css/bootstrap.min.css">
+    <link rel="stylesheet" href="receptionist.css">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.3/css/all.min.css">
+</head>
+<body>
+
+<div class="dashboard-header">
+    <h2>Dashboard</h2>
+</div>
+
+<div class="container">
+    <div class="flex-container">
+        <div class="sidebar">
+            <div class="top-button">
+                <i class="fas fa-user"></i> <p> Physician </p>
+            </div>
+            
+            <div id="main">
+                <a href="physician.php"><i class="fas fa-arrow-left"></i> Back</a>
+            </div>
+        </div>
+
+        <div class="content">
+            <div class="card-container">
+                <div class="card">
+                    <div class="card-body">
+                        <h5 class="card-title">Create Appointment</h5>
+                        <p>Create a new appointment for the patient.</p>
+                        <a href="create_app.php" class="btn btn-success">Click here</a>
+
+                    </div>
+                </div>
+
+                <div class="card">
+                    <div class="card-body">
+                        <h5 class="card-title">View Appointment History</h5>
+                        <p>View and manage your existing appointments.</p>
+                        <a href="view_app.php" class="btn btn-success">Click here</a>
+
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
+
+</body>
+</html>
