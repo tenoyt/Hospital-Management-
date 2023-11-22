e2ba7250af67efc02c30ad9d8ad43373a2ef9ed9
rushirajsinh rathod <109704652+rushirajrathod@users.noreply.github.com>
2023-11-21 23:38:31 -0500

    Add files via upload

---
+++ room.php
@@ -0,0 +1,62 @@
+<!DOCTYPE html>
+    <html lang="en">
+    <head>
+        <title> Room Details </title>
+        <link rel="stylesheet" type="text/css" href="https://cdn.jsdelivr.net/npm/bootstrap@4.6.2/dist/css/bootstrap.min.css">
+        <link rel="stylesheet" href="room.css">
+        <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.3/css/all.min.css">
+    </head>
+    <body>
+    <div class="dashboard-header">
+            <h2> Add patient room information</h2>
+        </div>
+    
+    <div class="sidebar">
+        <!-- New non-clickable and static button in the navbar -->
+        <div class="top-button">
+            <i class="fas fa-user"></i>  
+            <p>Receptionist</p>
+        </div>
+    
+        
+        <!-- Sidebar links -->
+        <div id="main">
+             <a href="receptionist.php"><i class="fas fa-arrow-left"></i> Back</a>
+        </div>
+        
+    </div>
+
+    <div class = "container">
+        <h3 style = "justify-content: center;">Room details</h3>
+        <form>
+          <div class="form-group">
+            <label for="formGroupExampleInput">Room ID</label>
+            <input type="text" class="form-control" id="formGroupExampleInput" placeholder="Input">
+          </div>
+          <div class="form-group">
+            <label for="formGroupExampleInput2">Bed ID</label>
+            <input type="text" class="form-control" id="formGroupExampleInput2" placeholder="Input">
+          </div>
+          <div class="form-group">
+            <label for="formGroupExampleInput">Patient ID</label>
+            <input type="text" class="form-control" id="formGroupExampleInput" placeholder="Input">
+          </div>
+          <div class="form-group">
+            <label for="formGroupExampleInput2">Room Number</label>
+            <input type="text" class="form-control" id="formGroupExampleInput2" placeholder="Input">
+          </div>
+          <div class="form-group">
+            <label for="formGroupExampleInput2">Room Type</label>
+            <input type="text" class="form-control" id="formGroupExampleInput2" placeholder="Input">
+          </div>
+          <div class="form-group">
+            <label for="formGroupExampleInput2">Occupancy Status</label>
+            <input type="text" class="form-control" id="formGroupExampleInput2" placeholder="Input">
+          </div>
+          
+        </form>
+        <button type="submit" class="btn btn-primary">Submit</button>
+    </div>
+
+    </body>
+    </html>
