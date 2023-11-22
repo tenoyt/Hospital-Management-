13fc32f7e000284b14c6f593e2049bf391c25339
rushirajsinh rathod <109704652+rushirajrathod@users.noreply.github.com>
2023-11-22 01:59:20 -0500

    Add files via upload

---
+++ lab_report.php
@@ -0,0 +1,50 @@
+<!DOCTYPE html>
+    <html lang="en">
+    <head>
+        <title> DBMS </title>
+        <link rel="stylesheet" type="text/css" href="https://cdn.jsdelivr.net/npm/bootstrap@4.6.2/dist/css/bootstrap.min.css">
+        <link rel="stylesheet" href="appointment.css">
+        <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.3/css/all.min.css">
+    </head>
+    <body>
+    <div class="dashboard-header">
+            <h2> Lab Report</h2>
+        </div>
+    
+    <div class="sidebar">
+        <!-- New non-clickable and static button in the navbar -->
+        <div class="top-button">
+            <i class="fas fa-user"></i>  
+            <p>Physician</p>
+        </div>
+    
+        
+        <!-- Sidebar links -->
+        <div id="main">
+             <a href="physician.php"><i class="fas fa-arrow-left"></i> Back</a> 
+        </div>
+        
+    </div>
+
+    <div class = "container">
+        <h3 style = "justify-content: center;">Generate Lab Report</h3>
+        <form>
+          <div class="form-group">
+            <label for="formGroupExampleInput"> Report ID</label>
+            <input type="text" class="form-control" id="formGroupExampleInput" placeholder="Input">
+          </div>
+          <div class="form-group">
+            <label for="formGroupExampleInput2">Patient ID</label>
+            <input type="text" class="form-control" id="formGroupExampleInput2" placeholder="Input">
+          </div>
+          <div class="form-group">
+            <label for="formGroupExampleInput"> Report Details </label>
+            <input type="text" class="form-control" id="formGroupExampleInput" placeholder="Input">
+          </div>
+         
+        </form>
+        <button type="submit" class="btn btn-primary">Submit</button>
+    </div>
+
+    </body>
+    </html>
