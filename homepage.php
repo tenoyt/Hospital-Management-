fe4eb39ad5f9847a18d387ba591584c30e7a62b6
rushirajsinh rathod <109704652+rushirajrathod@users.noreply.github.com>
2023-11-22 22:37:08 -0500

    Update homepage.php

--- homepage.php
+++ homepage.php
@@ -1,69 +1,81 @@
-<html>
+<!DOCTYPE html>
+<html lang="en">
+
 <head>
-    <title> homepage </title>
-    <link rel="stylesheet" type="text/css"
-        href="https://cdn.jsdelivr.net/npm/bootstrap@4.6.2/dist/css/bootstrap.min.css">
-    <link rel="stylesheet" href="homepage.css">
+    <title>Homepage</title>
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" type="text/css" href="https://cdn.jsdelivr.net/npm/bootstrap@4.6.2/dist/css/bootstrap.min.css">
+    <link rel="stylesheet" href="receptionist.css">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.3/css/all.min.css">
 </head>
 <body>
-    <div class="sidebar">
-            <div id="main">
-                <a href="receptionist.php">Receptionist</a>
-                <a href="eventOne.php">Physician</a>
-                <a href="registered.php">Nurse</a>
-            </div>
-            <div id="bottom">
-                <a href="costcentre.php">Cost Centre</a>
-        </div>
-    </div>
-        <p id="datetime"></p>
-        <script>
-            var now = new Date();
-            var datetime = now.toLocaleString();
-            document.getElementById("datetime").innerHTML = datetime;
-        </script>
-    <div class='home'>
-            <p>Sunshine Hospital</p>
-    </div>
-    <div id="announcement">
-        <p>Announcements</p>
+
+<div class="dashboard-header">
+    
+    
+<h2> Homepage </h2>
+
 </div>
-    <div class="content">
-            <div class="contact-info">
-                <div class="card">
-                <i class="card-info far fa-envelope"></i>
-                <div id="card1">
-                    <ul>
-                        <h1>Receptionist</h1>
-                        <li> Meeting at 1:00 PM </li>
-                        <li> Venue: Confrence Room </li>
-                        <li> Date: November 27th, 2023 </li>
-                    </ul>
+
+
+
+<div class="container">
+    <div class="flex-container">
+        <div class="sidebar">
+            <div class="top-button">
+                <i class="fas fa-user"></i> <p> Users </p>
+            </div>
+            
+            <div id="main">
+                <a href="receptionist.php"> Receptionist</a>
+                <a href="physician.php"> Physician</a>
+                <a href="nurse.php"> Nurse </a>
+                <a href="costcentre.php"> Cost Centre </a>
+
+
+            </div>
+        </div>
+
+        <div class="content">
+            <div class="card-container">
+                <!-- Announcements Card -->
+                <div class="card bg-success text-white">
+                    <div class="card-body">
+                        <h5 class="card-title">Announcements</h5>
+                        <p class="card-text">
+                            <!-- Announcement Content -->
+                            1. <strong>Office Closure:</strong> The office will be closed on [date] due to [reason]. We apologize for any inconvenience and appreciate your understanding.
+                            <br><br>
+                            2. <strong>New Policies or Procedures:</strong> We have implemented new procedures to enhance our services. Please review the updated policies on our website.
+                            <br><br>
+                            3. <strong>Welcome Message:</strong> Welcome to our receptionist dashboard! We are excited to serve you. If you have any questions, feel free to contact us.
+                            <br><br>
+                        </p>
+                    </div>
+                </div>
+
+                <!-- Events Card -->
+                <div class="card bg-success text-white">
+                    <div class="card-body">
+                        <h5 class="card-title">Events</h5>
+                        <p class="card-text">
+                            <!-- Event Content -->
+                            1. <strong>Upcoming Meetings:</strong> Monthly Staff Meeting - [date] at [time]. Location: [meeting room].
+                            <br><br>
+                            2. <strong>Training Sessions:</strong> Customer Service Training - [date] from [start time] to [end time]. All staff members are required to attend.
+                            <br><br>
+                            3. <strong>Celebrations and Recognitions:</strong> Employee of the Month Celebration - Join us in congratulating [employee name] on their outstanding performance.
+                            <br><br>
+                        </p>
+                    </div>
                 </div>
             </div>
-    <div class="card">
-            <i class="card-info far fa-envelope"></i>
-            <div id="card2">
-            <ul>
-            <h1>Physician</h1>
-                <li> Meeting at 3:00 PM </li>
-                <li> Venue: Main Hall </li>
-                <li> Date: November 26th, 2023 </li>
-            </ul>
-            </div>
-        </div>
-    <div class="card">
-            <i class="card-info far fa-envelope"></i>
-            <div id="card3">
-            <ul>
-            <h1>Nurse</h1>
-                <li> Meeting at 10:00 AM </li>
-                <li> Venue: Meeting Room 1 </li>
-                <li> Date: November 25th, 2023 </li>
-            </ul>
         </div>
     </div>
 </div>
-</div>
+
 </body>
 </html>
