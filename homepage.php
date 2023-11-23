6d9d5b2d7aac9af277334452c801740fd872e322
rushirajsinh rathod <109704652+rushirajrathod@users.noreply.github.com>
2023-11-22 23:28:56 -0500

    Update homepage.php

--- homepage.php
+++ homepage.php
@@ -6,76 +6,39 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="stylesheet" type="text/css" href="https://cdn.jsdelivr.net/npm/bootstrap@4.6.2/dist/css/bootstrap.min.css">
-    <link rel="stylesheet" href="receptionist.css">
+    <link rel="stylesheet" href="homepage.css">
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.3/css/all.min.css">
 </head>
+
 <body>
+    <a class="weatherwidget-io" href="https://forecast7.com/en/43d90n78d87/oshawa/" data-label_1="OSHAWA"
+        data-label_2="WEATHER" data-icons="Climacons Animated" data-mode="Current" data-days="3" data-theme="kiwi">OSHAWA
+        WEATHER</a>
+    <script>
+        !function (d, s, id) { var js, fjs = d.getElementsByTagName(s)[0]; if (!d.getElementById(id)) { js = d.createElement(s); js.id = id; js.src = 'https://weatherwidget.io/js/widget.min.js'; fjs.parentNode.insertBefore(js, fjs); } }(document, 'script', 'weatherwidget-io-js');
+    </script>
 
-<div class="dashboard-header">
-    
-    
-<h2> Homepage </h2>
-
-</div>
-
-
-
-<div class="container">
-    <div class="flex-container">
-        <div class="sidebar">
-            <div class="top-button">
-                <i class="fas fa-user"></i> <p> Users </p>
-            </div>
-            
-            <div id="main">
-                <a href="receptionist.php"> Receptionist</a>
-                <a href="physician.php"> Physician</a>
-                <a href="nurse.php"> Nurse </a>
-                <a href="costcentre.php"> Cost Centre </a>
-
-
-            </div>
-        </div>
-
-        <div class="content">
-            <div class="card-container">
-                <!-- Announcements Card -->
-                <div class="card bg-success text-white">
-                    <div class="card-body">
-                        <h5 class="card-title">Announcements</h5>
-                        <p class="card-text">
-                            <!-- Announcement Content -->
-                            1. <strong>Office Closure:</strong> The office will be closed on [date] due to [reason]. We apologize for any inconvenience and appreciate your understanding.
-                            <br><br>
-                            2. <strong>New Policies or Procedures:</strong> We have implemented new procedures to enhance our services. Please review the updated policies on our website.
-                            <br><br>
-                            3. <strong>Welcome Message:</strong> Welcome to our receptionist dashboard! We are excited to serve you. If you have any questions, feel free to contact us.
-                            <br><br>
-                        </p>
-                    </div>
+    <div class="content">
+        <div class="flex-container">
+            <div class="sidebar">
+                <div class="top-button">
+                    <i class="fas fa-user"></i> <p> Users </p>
                 </div>
-
-                <!-- Events Card -->
-                <div class="card bg-success text-white">
-                    <div class="card-body">
-                        <h5 class="card-title">Events</h5>
-                        <p class="card-text">
-                            <!-- Event Content -->
-                            1. <strong>Upcoming Meetings:</strong> Monthly Staff Meeting - [date] at [time]. Location: [meeting room].
-                            <br><br>
-                            2. <strong>Training Sessions:</strong> Customer Service Training - [date] from [start time] to [end time]. All staff members are required to attend.
-                            <br><br>
-                            3. <strong>Celebrations and Recognitions:</strong> Employee of the Month Celebration - Join us in congratulating [employee name] on their outstanding performance.
-                            <br><br>
-                        </p>
-                    </div>
+                <div id="main">
+                    <a href="receptionist.php"> Receptionist</a>
+                    <a href="physician.php"> Physician</a>
+                    <a href="nurse.php"> Nurse </a>
+                    <a href="costcentre.php"> Cost Centre </a>
                 </div>
+            </div>
+            <div class="main">
+                <p class="aesthetic-text">Welcome to the Sunshine Hospital !</p>
+                <p class="sub-text">Efficiently managing healthcare services with our state-of-the-art system.</p>
             </div>
         </div>
     </div>
-</div>
 
 </body>
+
 </html>
