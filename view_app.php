bee22b0e0e869b9ac5e01ccf719e6b223650a869
ShivamPatel1408 <146132518+ShivamPatel1408@users.noreply.github.com>
2023-11-24 14:30:03 -0500

    Update view_app.php

--- view_app.php
+++ view_app.php
@@ -28,26 +28,29 @@
                     <table class="table table-striped">
                         <thead>
                             <tr>
-                                <th>ID</th>
-                                <th>NAME</th>
-                                <th>DESCRIPTION</th>
-                                <th>COST</th>
+                                <th>APPOINTMENT ID</th>
+                                <th>PATIENT ID</th>
+                                <th>PHYSICIAN ID</th>
+                                <th>APPOINTMENT_DATE</th>
+                                <th>APPOINTMENT_TYPE</th>
+                                <th>APPOINTMENT_NOTE</th>
                             </tr>
                         </thead>
                         <tbody>
                             <?php
-                            $conn = mysqli_connect("localhost", "root", "", "project");
+                            $conn = mysqli_connect("localhost", "root", "1234", "project");
                             if ($conn->connect_error) {
                                 die("Connection Failed:" . $conn->connect_error);
                             }
-                            $sql = "SELECT COST_CENTRE_ID, ITEM_NAME, ITEM_DESCRIPTION, ITEM_COST FROM COST_CENTRE";
+                            $phyID = $_POST['PHYID'];
+                            $sql = "SELECT APPOINTMENT_ID, PATIENT_ID, PHYSICIAN_ID, APPOINTMENT_DATE, APPOINTMENT_TYPE, APPOINTMENT_NOTE FROM APPOINTMENTS where PHYSICIAN_ID = '$phyID'";
                             $result = $conn->query($sql);
                             if ($result->num_rows > 0) {
                                 while ($row = $result->fetch_assoc()) {
-                                    echo "<tr><td>" . $row["COST_CENTRE_ID"] . "</td><td>" . $row["ITEM_NAME"] . "</td><td>" . $row["ITEM_DESCRIPTION"] . "</td><td>" . $row["ITEM_COST"] . "</td></tr>";
+                                    echo "<tr><td>" . $row["APPOINTMENT_ID"] . "</td><td>" . $row["PATIENT_ID"] . "</td><td>" . $row["PHYSICIAN_ID"] . "</td><td>" . $row["APPOINTMENT_DATE"] . "</td><td>" . $row["APPOINTMENT_TYPE"] .  "</td><td>" . $row["APPOINTMENT_NOTE"] . "</td></tr>";
                                 }
                             } else {
-                                echo "<tr><td colspan='4'>0 result</td></tr>";
+                                echo "<tr><td colspan='6'>0 result</td></tr>";
                             }
                             $conn->close();
                             ?>
