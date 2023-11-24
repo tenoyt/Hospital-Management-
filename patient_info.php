5b72fee7bc26e7ca99c6290ba6dab13c200d99c4
ShivamPatel1408 <146132518+ShivamPatel1408@users.noreply.github.com>
2023-11-24 14:24:01 -0500

    Update patient_info.php

--- patient_info.php
+++ patient_info.php
@@ -7,19 +7,18 @@
         <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.3/css/all.min.css">
     </head>
     <body>
+    <form action="RPatient_Info.php" method="post">
     <div class="dashboard-header">
             <h2> Add patient details</h2>
         </div>
     
     <div class="sidebar">
-        <!-- New non-clickable and static button in the navbar -->
         <div class="top-button">
             <i class="fas fa-user"></i>  
             <p>Receptionist</p>
         </div>
     
         
-        <!-- Sidebar links -->
         <div id="main">
        
              <a href="receptionist.php"><i class="fas fa-arrow-left"></i> Back</a>
@@ -33,44 +32,40 @@
         <h3 style = "justify-content: center;">Invoice form</h3>
         <form>
           <div class="form-group">
-            <label for="formGroupExampleInput">Patient ID</label>
-            <input type="text" class="form-control" id="formGroupExampleInput" placeholder="Input">
-          </div>
-          <div class="form-group">
             <label for="formGroupExampleInput2">Name</label>
-            <input type="text" class="form-control" id="formGroupExampleInput2" placeholder="Input">
+            <input type="text" name="NAME" class="form-control">
           </div>
           <div class="form-group">
             <label for="formGroupExampleInput">Address</label>
-            <input type="text" class="form-control" id="formGroupExampleInput" placeholder="Input">
+            <input type="text" name="ADDRESS" class="form-control">
           </div>
           <div class="form-group">
             <label for="formGroupExampleInput2">City</label>
-            <input type="text" class="form-control" id="formGroupExampleInput2" placeholder="Input">
+            <input type="text" name="CITY" class="form-control">
           </div>
           <div class="form-group">
             <label for="formGroupExampleInput2">Phone No.</label>
-            <input type="text" class="form-control" id="formGroupExampleInput2" placeholder="Input">
+            <input type="text" name="PHONE" class="form-control">
           </div>
           <div class="form-group">
             <label for="formGroupExampleInput2">Sex</label>
-            <input type="text" class="form-control" id="formGroupExampleInput2" placeholder="Input">
+            <input type="text" name="SEX" class="form-control">
           </div>
           <div class="form-group">
             <label for="formGroupExampleInput2">HCN</label>
-            <input type="text" class="form-control" id="formGroupExampleInput2" placeholder="Input">
+            <input type="text" name="HCN" class="form-control">
           </div>
           <div class="form-group">
             <label for="formGroupExampleInput2">Admitted Date</label>
-            <input type="text" class="form-control" id="formGroupExampleInput2" placeholder="Input">
+            <input type="date" name="ADATE" class="form-control">
           </div>
           <div class="form-group">
             <label for="formGroupExampleInput2">Discharge Date</label>
-            <input type="text" class="form-control" id="formGroupExampleInput2" placeholder="Input">
+            <input type="date" name="DATE" class="form-control">
           </div>
           <div class="form-group">
             <label for="formGroupExampleInput2">Bed ID</label>
-            <input type="text" class="form-control" id="formGroupExampleInput2" placeholder="Input">
+            <input type="text" name="BEDID" class="form-control">
           </div>
 
           
